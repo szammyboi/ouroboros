0a4b8f08300f0cf43d19fde87a8a4dd67ad9c82c
Joshua <jlabasbas24@gmail.com>
2026-02-05 21:38:32 -0500

    Base struct

---
+++ src/physics/Object.h
@@ -0,0 +1,13 @@
+#include <glm/glm.hpp>
+
+namespace Ouroboros{
+    struct Body
+    {
+        glm::vec3 loc{0.0};
+        float mass{1.0};
+        float invMass{1.0};
+        glm::vec3 vel{0.0};
+        glm::vec3 force{0.0};
+       
+    };
+}
