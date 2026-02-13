aec8cc956a67aae2a28f605b8d7dab44c002f000
Sam Morse <szammyboi@gmail.com>
2026-02-13 03:18:16 -0500

    Fixed Apple Issue

--- src/ui/menubar.cpp
+++ src/ui/menubar.cpp
@@ -14,38 +14,39 @@ void MenuBar::OnDetach()
 }
 
 void MenuBar::OnUpdate()
-{	
+{
+#ifndef __APPLE__
 	const int drag_threshold = 2;
 	static bool dragging = false;
 	static RECT initialWindowRect;
 	static POINT initialMousePosition;
-
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-#ifndef __APPLE__
 	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, GetSystemMetrics(SM_CYFRAME)));
 #endif
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
 	if (ImGui::BeginMainMenuBar())
 	{
 #ifndef __APPLE__
 		if (dragging)
-		{	
+		{
 			POINT mouseNew;
 			GetCursorPos(&mouseNew);
 
 			int dx = mouseNew.x - initialMousePosition.x;
 			int dy = mouseNew.y - initialMousePosition.y;
-			
-			if (abs(dx) > drag_threshold || abs(dy) > drag_threshold) {
+
+			if (abs(dx) > drag_threshold || abs(dy) > drag_threshold)
+			{
 				SetWindowPos(m_Window.GetHWND(), NULL,
-					initialWindowRect.left + dx,
-					initialWindowRect.top + dy,
-					0, 0,
-					SWP_NOZORDER | SWP_NOSIZE | SWP_NOACTIVATE
-				);
+				    initialWindowRect.left + dx,
+				    initialWindowRect.top + dy,
+				    0, 0,
+				    SWP_NOZORDER | SWP_NOSIZE | SWP_NOACTIVATE);
 			}
 		}
 
-		if (!dragging && ImGui::IsWindowHovered() && ImGui::IsMouseClicked(0)) {
+		if (!dragging && ImGui::IsWindowHovered() && ImGui::IsMouseClicked(0))
+		{
 			dragging = true;
 
 			GetCursorPos(&initialMousePosition);
@@ -60,7 +61,7 @@ void MenuBar::OnUpdate()
 		ImGui::PopStyleVar();
 		ImGui::SetCursorPosY(-GetSystemMetrics(SM_CYFRAME));
 #endif
-		
+
 		ImGui::AlignTextToFramePadding();
 		if (ImGui::BeginMenu("Settings"))
 		{
